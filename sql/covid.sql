06eab6ff84c0d3a2f39bf3bfb8f923100d3663cf
luoqianyi <3228077562@qq.com>
2022-06-20 20:37:52 +0800

    fix:sql文件上传

--- sql/covid.sql
+++ sql/covid.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : 小落
  Source Server Type    : MySQL
- Source Server Version : 50735
- Source Host           : localhost:3306
+ Source Server Version : 50727
+ Source Host           : 1.116.189.219:3306
  Source Schema         : covid
 
  Target Server Type    : MySQL
- Target Server Version : 50735
+ Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 14/06/2022 15:19:54
+ Date: 20/06/2022 20:37:06
 */
 
 SET NAMES utf8mb4;
@@ -34,23 +34,20 @@ CREATE TABLE `emp_health`  (
   `depart` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `de`(`depart`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of emp_health
 -- ----------------------------
-INSERT INTO `emp_health` VALUES (2, '刘先生', 1, 15143355464, 36.5, '否', '正常', '', '2021-03-31 14:03:52', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (3, '吴雷师', 0, 15151549751, 37.0, '否', '正常', '无', '2021-03-13 20:16:30', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (4, '刘腾键', 1, 13415135795, 36.5, '否', '正常', '', '2021-03-13 19:54:40', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (5, '刘腾键', 1, 13415135795, 36.5, '否', '感冒', '', '2021-03-13 19:54:40', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (6, '刘胜虎', 1, 123414234123, 39.0, '否', '低烧', '', '2021-04-05 22:41:39', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (7, '王继红', 0, 13513534534, 38.0, '否', '与新冠肺炎有关的其他症状，如流涕，咽痛，肌痛，腹泻等', '', '2021-04-06 13:39:38', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (8, '刘小姐', 1, 231123123, 23.0, '是', '呼吸困难', '', '2021-04-06 15:34:42', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (9, '111', 1, 111111111, 11.0, '111', '正常', '11', '2021-04-10 16:03:01', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (10, '刘渲', 1, 13514124567, 37.0, '否', '正常', '', '2021-04-15 14:29:15', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (11, '王佳', 0, 13512314534, 37.5, '否', '正常', '', '2021-04-15 14:42:55', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (12, '刘腾键', 1, 13512365456, 36.5, '否', '正常', '', '2021-04-15 14:46:49', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_health` VALUES (13, '123', 1, 18234567891, 36.0, '否', '正常', '', '2022-06-14 11:14:56', '新乡市红旗区河南科技学院数据3班');
+INSERT INTO `emp_health` VALUES (1, 'Violet North', 1, 13842562340, 37.1, '是', '乏力', '', '2022-06-17 15:15:33', '美国某乡村');
+INSERT INTO `emp_health` VALUES (2, 'Wordsworth Young', 1, 18848265378, 37.5, '是', '呼吸困难', '', '2022-06-17 15:16:19', '美国某乡村');
+INSERT INTO `emp_health` VALUES (3, 'さかたしょうこ', 1, 13342814430, 35.0, '否', '与新冠肺炎有关的其他症状，如流涕，咽痛，肌痛，腹泻等', '', '2022-06-17 15:17:51', '日本某乡村');
+INSERT INTO `emp_health` VALUES (4, 'こうらさちこ', 1, 13417071744, 36.1, '是', '呼吸困难', '', '2022-06-17 15:19:16', '日本某乡村');
+INSERT INTO `emp_health` VALUES (5, '서칠현', 1, 13178457186, 38.1, '是', '与新冠肺炎有关的其他症状，如流涕，咽痛，肌痛，腹泻等', '', '2022-06-17 15:20:22', '韩国某乡村');
+INSERT INTO `emp_health` VALUES (6, '刘兴修', 1, 15987046410, 36.5, '否', '正常', '', '2022-06-17 15:21:12', '中国某乡村');
+INSERT INTO `emp_health` VALUES (8, '李平', 0, 18705027971, 37.4, '是', '乏力', '', '2022-06-17 15:23:18', '中国某乡村');
+INSERT INTO `emp_health` VALUES (9, '李洋', 1, 13843595252, 37.2, '否', '咳嗽', '', '2022-06-13 00:00:00', '中国某乡村');
+INSERT INTO `emp_health` VALUES (10, '黄大', 1, 13234546422, 36.0, '否', '与新冠肺炎有关的其他症状，如流涕，咽痛，肌痛，腹泻等', '无', '2022-06-17 00:00:00', '天下');
 
 -- ----------------------------
 -- Table structure for emp_iden
@@ -69,22 +66,20 @@ CREATE TABLE `emp_iden`  (
   `depart` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `emp_iden_ibfk_1`(`depart`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of emp_iden
 -- ----------------------------
-INSERT INTO `emp_iden` VALUES (1, '李先生', 1, '440524196002152100', '2021-12-31', '广州市第一附属医院', 13457896457, '2020-11-14 15:31:44', '治愈', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (2, '蔡二思', 1, '440634199004050300', '2021-12-24', '深圳市福田区第一医院', NULL, '2021-03-03 11:33:31', '治愈', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (3, '王金枝', 0, NULL, '2022-01-12', '深圳市福田区第一医院', 123123123123, NULL, '治愈', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (4, '季王红', 0, '440578199505150500', '2022-02-15', '佛山市三水人民医院', 13411234457, NULL, '治愈', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (5, '李王思', 1, '440578199902150340', '2022-03-23', '佛山市三水人民医院', 13453456785, '2021-04-06 16:05:11', '疑似', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (6, '王二会', 1, '440582198005040800', '2022-03-03', '深圳市中心医院', 13457891231, '2020-04-09 16:22:26', '疑似', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (7, '于雪师', 1, '440578198905213551', '2020-12-12', '深圳市中心医院', 1581456785, '2021-04-06 16:05:11', '死亡', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (10, '阿斯顿', 0, NULL, '2021-12-01', '汕头市李嘉诚医院', 13531342457, '2021-04-02 14:49:15', '治愈', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (11, '士大夫', 0, '34124', '2021-12-14', '广州市暨南大学第一附属医院', 13411213257, '2021-04-02 14:49:32', '死亡', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (12, '吴开师', 1, '440578199807050321', '2021-12-12', '佛山市三水人民医院', 13412823457, '2021-04-06 16:05:11', '疑似', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_iden` VALUES (13, '张三', 1, NULL, '2022-06-12', '无名', 121, '2022-06-13 15:25:52', NULL, '新乡市红旗区河南科技学院数据3班');
+INSERT INTO `emp_iden` VALUES (2, '韩一龙', 1, '411222200101293900', '2022-05-15', '新百医院', 15513928374, '2022-06-17 16:14:53', '疑似', '新乡红旗区河南科技学院');
+INSERT INTO `emp_iden` VALUES (3, '罗某人', 1, '411524200108190000', '2022-06-12', '新乡医学院', 18073579794, '2022-06-17 16:30:11', '治愈', '新乡红旗区河南科技学院');
+INSERT INTO `emp_iden` VALUES (4, '侯某人', 0, '411524200109210000', '2022-06-13', '新乡医学院', 18073562211, '2022-06-17 16:31:37', '疑似', '新乡红旗区大数据大厦');
+INSERT INTO `emp_iden` VALUES (5, '黄某人', 1, '411524200109118818', '2022-05-17', '市中心医院', 18032100218, '2022-06-17 16:33:07', '死亡', '光山某乡');
+INSERT INTO `emp_iden` VALUES (6, '李雪羽', 0, '410102200103070492', '2022-04-04', '当下乱世医院', 15717502462, '2022-06-17 16:34:59', '确诊', '未名小区');
+INSERT INTO `emp_iden` VALUES (7, '吴宇', 1, '410102200103075816', '2022-03-14', '救死扶伤医院', 15717502461, '2022-06-17 16:36:14', '确诊', '未名小区');
+INSERT INTO `emp_iden` VALUES (8, '张简若秋', 0, '410102200103074979', '2022-02-21', '股份有限医院', 15717502499, '2022-06-17 16:38:16', '确诊', '无名小区');
+INSERT INTO `emp_iden` VALUES (9, '侯若桐', 0, '165544564564898', '2022-05-31', '三附院', 15637920887, '2022-06-17 21:50:06', '疑似', '柳青路');
+INSERT INTO `emp_iden` VALUES (10, '黄怀龙', 1, '412325197802102340', '2022-05-09', '三附院', 13723451432, '2022-06-17 21:50:49', '治愈', '无');
 
 -- ----------------------------
 -- Table structure for emp_is
@@ -105,15 +100,13 @@ CREATE TABLE `emp_is`  (
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `depart` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of emp_is
 -- ----------------------------
-INSERT INTO `emp_is` VALUES (1, '黎香湖', 1, 12341231231, 35.00, '酒店隔离', '广州市广州南站如家酒店', '2021-12-06 00:00:00', '2021-12-20 00:00:00', '广东汕头', '广东东莞', NULL, '新乡市红旗区河南科技学院');
-INSERT INTO `emp_is` VALUES (2, '王老二', 1, 13545479653, 38.50, '酒店集中隔离', '广州市火车站快捷酒店', '2021-12-14 00:00:00', '2021-12-28 00:00:00', '广东汕头', '广东东莞', '', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_is` VALUES (3, '蔡司', 1, 13452342356, 36.60, '酒店集中隔离', '深圳市宝安区平和小区', '2021-12-23 00:00:00', '2022-01-06 00:00:00', '重庆市', '湖南长沙', '', '新乡市红旗区河南科技学院数据3班');
-INSERT INTO `emp_is` VALUES (5, '蔡司', 1, 13452342356, 35.60, '居家隔离', '深圳市宝安区平和小区', '2021-12-09 00:00:00', '2021-12-23 00:00:00', '重庆市', '湖南长沙', '', '新乡市红旗区河南科技学院数据3班');
+INSERT INTO `emp_is` VALUES (1, '张德', 1, 15683748367, 36.60, '居家隔离', '上阳路盛阳酒店', '2022-06-15 00:00:00', '2022-06-29 00:00:00', '北京', '郑州', '', '新乡');
+INSERT INTO `emp_is` VALUES (2, '张三', 1, 13734343213, 37.00, '自费', '无', '2022-06-16 00:00:00', '2022-06-30 00:00:00', '无', '无', '无', '无');
 
 -- ----------------------------
 -- Table structure for material_manage
@@ -129,17 +122,19 @@ CREATE TABLE `material_manage`  (
   `cnum` bigint(20) DEFAULT NULL,
   `updateTime` datetime(0) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 134 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of material_manage
 -- ----------------------------
-INSERT INTO `material_manage` VALUES (113, 'KN94口罩', 150, '个', 1, '孙迪', 13415135777, '2021-03-31 13:54:36');
-INSERT INTO `material_manage` VALUES (114, 'N95口罩', 220, '个', 1, '孙迪', 13415135777, '2021-04-06 16:14:45');
-INSERT INTO `material_manage` VALUES (123, '防护服', 30, '个', 1, '王旭', 15148796568, '2021-04-22 14:21:56');
-INSERT INTO `material_manage` VALUES (125, '酒精消毒棉片', 200, '盒', 1, '李建', 13431357964, '2021-03-31 13:54:06');
-INSERT INTO `material_manage` VALUES (126, '抽纸纸巾', 20, '箱', 0, '李玉', 13534654675, '2021-04-22 19:29:30');
-INSERT INTO `material_manage` VALUES (133, '防护眼罩', 30, '个', 1, '杨迪', 123123123123, '2021-04-23 17:00:52');
+INSERT INTO `material_manage` VALUES (1, '口罩', 10000, '个', 1, '罗某人', 18073579791, '2022-06-17 21:49:56');
+INSERT INTO `material_manage` VALUES (2, '隔离衣', 2000, '件', 1, '李明', 13839097893, '2022-06-17 16:10:54');
+INSERT INTO `material_manage` VALUES (3, '防护面罩', 320, '箱', 1, '张丽', 15319380498, '2022-06-17 16:16:05');
+INSERT INTO `material_manage` VALUES (4, '疫苗', 3000, '盒', 1, '李一清', 18819387476, '2022-06-17 16:05:38');
+INSERT INTO `material_manage` VALUES (5, '消毒液', 4000, '箱', 1, '王洋', 18938748943, '2022-06-17 16:17:42');
+INSERT INTO `material_manage` VALUES (6, '医用防护服', 2500, '件', 1, '方晴', 13398579380, '2022-06-17 16:08:16');
+INSERT INTO `material_manage` VALUES (7, '医用酒精', 5000, '瓶', 1, '张思涵', 15683873933, '2022-06-17 16:08:59');
+INSERT INTO `material_manage` VALUES (8, '医用手套', 3000, '袋', 1, '冯好书', 18738748466, '2022-06-17 16:10:02');
 
 -- ----------------------------
 -- Table structure for user
@@ -150,12 +145,16 @@ CREATE TABLE `user`  (
   `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e');
-INSERT INTO `user` VALUES (2, '123', '202cb962ac59075b964b07152d234b70');
+INSERT INTO `user` VALUES (2, 'hhh', '4297f44b13955235245b2497399d7a93');
+INSERT INTO `user` VALUES (3, 'liuliu', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES (4, 'kong1', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES (5, 'kong1', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` VALUES (6, 'luoqianyi', 'e10adc3949ba59abbe56e057f20f883e');
 
 SET FOREIGN_KEY_CHECKS = 1;
